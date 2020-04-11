f6c8611616491b6ffa462b8b18d04f11271fc93d
Tuan Nguyen <vdtn359@gmail.com>
2020-04-11 16:12:28 +1000

    feat(animation): animate scroll button

--- app/web/src/components/common/navigation/ScrollTopButton.tsx
+++ app/web/src/components/common/navigation/ScrollTopButton.tsx
@@ -2,27 +2,20 @@ import styled from 'styled-components';
 import { Button } from 'semantic-ui-react';
 import React, { useEffect } from 'react';
 import { useStateRef } from 'src/utils/hooks/basic';
+import { AnimatePresence, motion } from 'framer-motion';
 
-const ScrollTop = styled(Button)`
+const ScrollTop = styled(motion.div)`
     display: inline-block;
-    background-color: #ff9800;
-    width: 50px;
-    height: 50px;
-    text-align: center;
-    border-radius: 4px;
     position: fixed;
     bottom: 30px;
     right: 30px;
-    transition: background-color 0.3s, opacity 0.5s, visibility 0.5s;
     z-index: 1000;
+`;
 
-    &:hover {
-        cursor: pointer;
-        background-color: #333;
-    }
-    &:active {
-        background-color: #555;
-    }
+const ScrollButton = styled(Button)`
+    width: 50px;
+    height: 50px;
+    text-align: center;
 `;
 export function ScrollTopButton() {
     const [isScrolled, setIsScrolled] = useStateRef(false);
@@ -41,18 +34,26 @@ export function ScrollTopButton() {
     }, []);
 
     return (
-        isScrolled() && (
-            <ScrollTop
-                circular
-                color={'orange'}
-                icon={'arrow up'}
-                onClick={() =>
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth',
-                    })
-                }
-            />
-        )
+        <AnimatePresence>
+            {isScrolled() && (
+                <ScrollTop
+                    initial={{ bottom: '-60px' }}
+                    animate={{ bottom: '30px' }}
+                    exit={{ bottom: '-60px' }}
+                >
+                    <ScrollButton
+                        circular
+                        color={'orange'}
+                        icon={'arrow up'}
+                        onClick={() =>
+                            window.scrollTo({
+                                top: 0,
+                                behavior: 'smooth',
+                            })
+                        }
+                    />
+                </ScrollTop>
+            )}
+        </AnimatePresence>
     );
 }
