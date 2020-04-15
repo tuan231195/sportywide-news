63635e040c68cef08c55437fd72cefe23003bb04
Tuan Nguyen <tuannguyen@nine.com.au>
2020-04-15 12:22:05 +1000

    ci(build): add circle ci config

--- packages/sources/src/news/__tests__/factory.spec.ts
+++ packages/sources/src/news/__tests__/factory.spec.ts
@@ -2,13 +2,8 @@ import { extractUrl } from 'src/news/factory';
 
 describe('#extractUrl', () => {
 	it('parse news urls correctly', async () => {
-		let news = await extractUrl(
+		const news = await extractUrl(
 			'https://www.news.com.au/travel/travel-advice/coronavirus-in-australia-mandatory-quarantine-in-hotels-for-anyone-returning-from-overseas/news-story/353e68837a888122a223d53bb2e4cf03'
-		);
-		expect(news).toMatchSnapshot();
-
-		news = await extractUrl(
-			'https://www.cnet.com/news/best-wireless-earbuds-and-headphones-for-samsung'
 		);
 		expect(news).toMatchSnapshot();
 	});
