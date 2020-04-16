94e485dec0d5e49f4de05217a0f63ec7efde8278
Tuan Nguyen <vdtn359@gmail.com>
2020-04-16 20:47:58 +1000

    feat(seo): add robots.txt

---
+++ app/web/src/pages/api/sitemap.xml.ts
@@ -0,0 +1,79 @@
+/*  eslint-disable @typescript-eslint/camelcase */
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getHandler } from 'src/api/handler';
+import { SitemapStream, streamToPromise, EnumChangefreq } from 'sitemap';
+import { es } from 'src/setup';
+import { NEWS_INDEX } from '@vdtn359/news-search';
+import { parseFields } from 'src/utils/search/fields';
+import { categoryMap } from 'src/utils/categories';
+import { subDays, startOfDay } from 'date-fns';
+import { isDevelopment } from 'src/utils/env';
+const handler = getHandler();
+handler.get(request);
+
+export default handler;
+
+async function request(req: NextApiRequest, res: NextApiResponse) {
+	res.setHeader('Content-Type', 'text/xml');
+	const stories = await fetchContentFromAPI();
+
+	const protocol = isDevelopment() ? 'http' : 'https';
+	const smStream = new SitemapStream({
+		hostname: `${protocol}://${req.headers.host}`,
+	});
+	for (const story of stories) {
+		smStream.write({
+			url: `/news/${story.slug}`,
+			lastmod: new Date(story.pubDate),
+		});
+	}
+	const pages = [
+		'/',
+		'/contact',
+		'/hot-news',
+		...Object.keys(categoryMap).map(
+			(category) => `/categories/${category.toLowerCase()}`
+		),
+	];
+	for (const page of pages) {
+		smStream.write({
+			url: page,
+			lastmod: startOfDay(subDays(new Date(), 1)),
+			changefreq: EnumChangefreq.DAILY,
+		});
+	}
+	smStream.end();
+	const sitemap = await streamToPromise(smStream).then((sm) => sm.toString());
+	res.write(sitemap);
+	res.end();
+}
+
+async function fetchContentFromAPI() {
+	const {
+		body: {
+			hits: { hits },
+		},
+	} = await es.search({
+		index: NEWS_INDEX,
+		size: 30,
+		body: {
+			query: {
+				match_all: {},
+			},
+			_source: ['slug', 'id', 'pubDate'],
+			sort: [
+				{
+					pubDate: {
+						order: 'desc',
+					},
+				},
+				{
+					id: {
+						order: 'desc',
+					},
+				},
+			],
+		},
+	});
+	return parseFields(hits);
+}
