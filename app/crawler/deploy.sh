0e9bf434e55f2b68a1bd4844e1c0f0abf76a7306
Tuan Nguyen <tuannguyen@nine.com.au>
2020-04-16 15:46:06 +1000

    ci(deploy): add missing region

--- app/crawler/deploy.sh
+++ app/crawler/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LAMBDA_VERSION="$(aws lambda list-tags --resource arn:aws:lambda:ap-southeast-2:923678104243:function:vdtn359-news-crawler --query 'Tags.version')"
+LAMBDA_VERSION="$(aws lambda list-tags --region ap-southeast-2 --resource arn:aws:lambda:ap-southeast-2:923678104243:function:vdtn359-news-crawler --query 'Tags.version')"
 PACKAGE_VERSION="$(cat package.json | jq '.version')"
 
 if [ "$PACKAGE_VERSION" != "$LAMBDA_VERSION" ]; then
