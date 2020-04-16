3e128986aec63d4e1d5153e18a8e2e1ba1105e76
Tuan Nguyen <tuannguyen@nine.com.au>
2020-04-16 15:37:46 +1000

    fix(ci): consolidate build process

--- app/web/deploy.sh
+++ app/web/deploy.sh
@@ -5,8 +5,6 @@ VERSION="$(cat ./package.json | jq -r .version)"
     docker pull registry.heroku.com/vdtn359-news/web:"$VERSION"
 } || {
   echo "Building web version $VERSION"
-  npm run build
-  npx mono-install --engine pnpm --install-dir produles --package-json ./package.json --package-lock ../../pnpm-lock.yaml --resolve --dry-run
   docker build -t registry.heroku.com/vdtn359-news/web:$VERSION -f Dockerfile --target prod .
   docker tag registry.heroku.com/vdtn359-news/web:$VERSION registry.heroku.com/vdtn359-news/web:latest
   docker push registry.heroku.com/vdtn359-news/web:$VERSION
--- app/worker/deploy.sh
+++ app/worker/deploy.sh
@@ -5,8 +5,6 @@ VERSION="$(cat ./package.json | jq -r .version)"
     docker pull registry.heroku.com/vdtn359-news/worker:"$VERSION"
 } || {
   echo "Building worker version $VERSION"
-  npm run build
-  npx mono-install --engine pnpm --install-dir produles --package-json ./package.json --package-lock ../../pnpm-lock.yaml --resolve --dry-run
   docker build -t registry.heroku.com/vdtn359-news/worker:$VERSION -f Dockerfile --target prod .
   docker tag registry.heroku.com/vdtn359-news/worker:$VERSION registry.heroku.com/vdtn359-news/worker:latest
   docker push registry.heroku.com/vdtn359-news/worker:$VERSION
