fc99df45c688af4e65eb891f6837b6b933749e5b
Tuan Nguyen <tuannguyen@nine.com.au>
2020-07-08 19:24:39 +1000

    fix(web): fixed int query

--- app/web/src/pages/api/hot-news.ts
+++ app/web/src/pages/api/hot-news.ts
@@ -5,13 +5,15 @@ import { NEWS_INDEX } from '@vdtn359/news-search';
 import { buildCommonQuery } from 'src/utils/search/query';
 import { FIELDS, parseFields } from 'src/utils/search/fields';
 import { getHandler } from 'src/api/handler';
+import { intQuery } from 'src/api/parse';
 const handler = getHandler();
 handler.get(request);
 
 export default handler;
 
 async function request(req: NextApiRequest, res: NextApiResponse) {
-	const commonNews = await search(req.query.size);
+	const size = intQuery(req.query.size, 20);
+	const commonNews = await search(size);
 	res.json(commonNews);
 }
 
