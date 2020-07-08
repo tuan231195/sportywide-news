334337f1ffc2f8c428911526675864ef68880e2f
Tuan Nguyen <tuannguyen@nine.com.au>
2020-07-08 18:54:39 +1000

    fix(test): fixed failing tests

--- packages/sources/src/news/__tests__/cnet-news.spec.ts
+++ packages/sources/src/news/__tests__/cnet-news.spec.ts
@@ -1,10 +1,12 @@
-import { CnetNews } from 'src/news';
+import { CnetNews, getRawText } from 'src/news';
 
 describe('#extractNews', () => {
 	it('should strip style and css and data attributes', async () => {
 		expect(
-			await CnetNews.extractUrl(
-				'https://www.cnet.com/news/every-livestream-concerts-coronavirus-shutdown'
+			getRawText(
+				await CnetNews.extractUrl(
+					'https://www.cnet.com/news/every-livestream-concerts-coronavirus-shutdown'
+				)
 			)
 		).toMatchSnapshot();
 	});
--- packages/sources/src/news/__tests__/techrepublic-news.spec.ts
+++ packages/sources/src/news/__tests__/techrepublic-news.spec.ts
@@ -1,10 +1,13 @@
+import { getRawText } from 'src/news/utils';
 import { TechRepublicNews } from 'src/news';
 
 describe('#extractNews', () => {
 	it('should strip style and css and data attributes', async () => {
 		expect(
-			await TechRepublicNews.extractUrl(
-				'https://www.techrepublic.com/article/pandemic-could-shift-tech-support-industry-to-telecommuting/#ftag=RSS56d97e7'
+			getRawText(
+				await TechRepublicNews.extractUrl(
+					'https://www.techrepublic.com/article/pandemic-could-shift-tech-support-industry-to-telecommuting/#ftag=RSS56d97e7'
+				)
 			)
 		).toMatchSnapshot();
 	});
